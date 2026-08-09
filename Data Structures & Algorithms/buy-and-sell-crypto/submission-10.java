65ace18ba325b6577c124627f4a0374ad46ef4fe
YatneshSokal <yatneshsokal@gmail.com>
2026-08-09 22:59:57 +0530

    Add: buy-and-sell-crypto - submission-10

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-10.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxp=0,minbuy=prices[0];
+        for(int sell:prices){
+            maxp=Math.max(maxp,sell-minbuy);
+            minbuy=Math.min(minbuy,sell);
+        }
+        return maxp;
+    }
+}
